b9e509325faf38ddb26f36a77101abcbb53cda6a
DavinderJolly <davinderrocks0786@gmail.com>
2021-10-04 17:14:32 +0530

    Added --export option
    added an option which lets you export alist of tests and their
    disscriptions from their docstrings to either csv or json formats

--- tests/test_testprog.py
+++ tests/test_testprog.py
@@ -3,20 +3,32 @@ import time
 
 
 def test_foo():
+    """Test.
+    First test
+    """
     time.sleep(2)
     assert True
 
 
 def test_bar():
+    """Test.
+    Second test
+    """
     time.sleep(2)
     assert True
 
 
 def test_baz():
+    """Test.
+    Third test
+    """
     time.sleep(2)
     assert False
 
 
-def test_bubz():
+def test_buz():
+    """Test.
+    Fourth test
+    """
     time.sleep(2)
     assert True
